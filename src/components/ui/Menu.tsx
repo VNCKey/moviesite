45148547c231e4e5a1d1cbf46a3806eb871beae4
LeonardoGrande <leofgh18@gmail.com>
2025-06-30 16:41:28 -0500

    Navbar Actualizado

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,33 +1,23 @@
-import { NavLink } from "react-router";
-import style from "../style/Menu.module.css"
+import { NavLink } from "react-router"; // tu decisión, aunque no recomendado
+import style from "../style/Menu.module.css";
 
 const Menu = () => {
-    return (
-        <nav className={style.nav}>
-            <div className={style.containerNav}>
-                <NavLink to={"/"}>PELICULAS</NavLink>
-                <div>
-                    <ul>    
-                        <li>
-                            <NavLink to={'/movies/filter'}>Filtrar Peliculas</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/genres'}>Géneros</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/actors'}>Actores</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/cines'}>Cines</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/movies/create'}>Crear Pelicula</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={style.nav}>
+      <div className={style.containerNav}>
+        <NavLink to={"/"}>
+          <img src="/imagenes/logo.png" alt="Peruflix Logo" className={style.logo} />
+        </NavLink>
+        <ul className={style.navLinks}>
+          <li><NavLink to={"/movies/filter"}>Filtrar Películas</NavLink></li>
+          <li><NavLink to={"/genres"}>Géneros</NavLink></li>
+          <li><NavLink to={"/actors"}>Actores</NavLink></li>
+          <li><NavLink to={"/cines"}>Cines</NavLink></li>
+          <li><NavLink to={"/movies/create"}>Crear Película</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Menu;
